d0f0ccde809d2d9353689806c76fa5e2e313f04f
Nabin Khanal <nabinkhanal@skiff.com>
2024-06-11 10:14:27 +0545

    send message using envelope

---
+++ psenvpub.c
@@ -0,0 +1,29 @@
+#include "zhelpers.h"
+#include <stdbool.h>
+
+int main()
+{
+    void *context = zmq_ctx_new();
+    assert(context);
+
+    void *publisher = zmq_socket(context, ZMQ_PUB);
+    assert(publisher);
+
+    int rc = zmq_bind(publisher, "tcp://*:5563");
+    assert(rc == 0);
+
+    while (true)
+    {
+        s_sendmore(publisher, "A");
+        s_send(publisher, "We would not like to see this.");
+
+        s_sendmore(publisher, "B");
+        s_send(publisher, "We would like to see this.");
+        sleep(1);
+    }
+
+    zmq_close(publisher);
+    zmq_ctx_destroy(publisher);
+
+    return 0;
+}
---
+++ psenvsub.c
@@ -0,0 +1,31 @@
+#include "zhelpers.h"
+
+int main(int argc, char **argv)
+{
+    int rc;
+
+    void *context = zmq_ctx_new();
+    assert(context);
+
+    void *subscriber = zmq_socket(context, ZMQ_SUB);
+    assert(subscriber);
+
+    rc = zmq_connect(subscriber, "tcp://localhost:5563");
+    assert(rc == 0);
+
+    rc = zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, argv[1], 1);
+    assert(rc == 0);
+
+    while (1)
+    {
+        char *address = s_recv(subscriber);
+        char *content = s_recv(subscriber);
+        printf("[%s] [%s]\n", address, content);
+        free(address);
+        free(content);
+    }
+
+    zmq_close(subscriber);
+    zmq_ctx_destroy(context);
+    return 0;
+}
