3a38b31e5f6baba4841297cfe5e30fc43b6f50f6
Nabin Khanal <nabinkhanal@skiff.com>
2024-06-17 10:15:06 +0545

    identity of socket

--- aarequest.c
+++ aarequest.c
@@ -1,19 +1,19 @@
 #include "zhelpers.h"
 
 int main() {
-  void *context = zmq_ctx_new();
+    void* context = zmq_ctx_new();
 
-  void *request = zmq_socket(context, ZMQ_REQ);
+    void* request = zmq_socket(context, ZMQ_REQ);
 
-  zmq_connect(request, "tcp://localhost:5556");
+    zmq_connect(request, "tcp://localhost:5556");
 
-  while (1) {
-    s_send(request, "Hello");
-    char *msg = s_recv(request);
-    printf("Received: [%s]\n", msg);
-    free(msg);
-    sleep(1);
-  }
-  zmq_close(request);
-  zmq_ctx_destroy(context);
+    while(1) {
+        s_send(request, "Hello");
+        char* msg = s_recv(request);
+        printf("Received: [%s]\n", msg);
+        free(msg);
+        sleep(1);
+    }
+    zmq_close(request);
+    zmq_ctx_destroy(context);
 }
